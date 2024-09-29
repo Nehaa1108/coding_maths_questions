085da4178033245b91af8c28a1cece437ae46753
Neha Gupta <127198995+Nehaa1108@users.noreply.github.com>
2024-09-29 14:04:12 -0400

    Add files via upload

---
+++ 1sumofseries.py
@@ -0,0 +1,15 @@
+
+
+
+def sumofseries(num):
+    for i in range(1,num+1):
+        return (num*(num+1)//2)
+
+print(sumofseries(5))
+
+
+n=int(input())
+sum=0
+for i in range(n+1):
+    sum=sum+i
+print(sum)
---
+++ 2areaoftriangle.py
@@ -0,0 +1,6 @@
+
+# 1/2*b*h
+base=float(input())
+height=float(input())
+area=1/2*base*height
+print(area)
