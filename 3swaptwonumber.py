a7a86587cdaae4605e831b61dfa0fc51ff993f28
Neha Gupta <127198995+Nehaa1108@users.noreply.github.com>
2024-10-01 14:31:08 -0400

    Add files via upload

---
+++ 3swaptwonumber.py
@@ -0,0 +1,16 @@
+num1=int(input())
+num2=int(input())
+temp=num1
+num1=num2
+num2=temp
+print(num1,num2)
+
+
+
+num1,num2=num2,num1
+print(num1,num2)
+
+num1=num1^num2
+num2=num1^num2
+num1=num1^num2
+print(num1,num2)
---
+++ 4convertkilo-mile.py
@@ -0,0 +1,3 @@
+kilo=float(input())
+miles=kilo*0.621371
+print(miles)
